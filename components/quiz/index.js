ed1807862741da087af8fca03659801a04fa9f2e
pendueles <contactocueto@gmail.com>
2026-06-15 13:18:56 +0200

    Create index.js

---
+++ components/quiz/index.js
@@ -0,0 +1,7 @@
+// components/quiz/index.js
+export { default as SwipeCard } from "./SwipeCard";
+export { default as ResultScreen } from "./ResultScreen";
+export { default as SubcatSummaryScreen } from "./SubcatSummaryScreen";
+export { default as BlockSummaryScreen } from "./BlockSummaryScreen";
+export { default as TotalSummaryScreen } from "./TotalSummaryScreen";
+export { default as PendingTasksScreen } from "./PendingTasksScreen";
