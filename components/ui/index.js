957b2aa9d8f0347405e2e618dc28d316cca32a2c
pendueles <contactocueto@gmail.com>
2026-06-15 13:12:07 +0200

    Create index.js

---
+++ components/ui/index.js
@@ -0,0 +1,5 @@
+// components/ui/index.js
+export { default as ResetButton } from "./ResetButton";
+export { default as HomeButton } from "./HomeButton";
+export { default as HexRadarTotal } from "./HexRadarTotal";
+export { default as SplashScreen } from "./SplashScreen";
