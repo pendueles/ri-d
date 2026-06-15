0f82af49cbf57826808e89010f49bd7c4479a9ad
pendueles <contactocueto@gmail.com>
2026-06-15 13:05:08 +0200

    Create index.js

---
+++ firebase/index.js
@@ -0,0 +1,11 @@
+// firebase/index.js
+export { db } from "./firebase";
+export {
+  useFirebaseStore, saveState, loadState, clearState,
+  getArtists, getFirebaseError, clearFirebaseError,
+  saveOneArtist, saveProject, saveArtists, deleteArtists, deleteProjectById,
+  getArtistUsers, saveArtistUsers, registerArtistUser,
+  getLabelUsers, saveLabelUsers,
+  getMgmtUsers, saveMgmtUsers,
+  startRealtimeSync,
+} from "./store";
