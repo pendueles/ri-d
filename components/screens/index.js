e1a588c745e15961a94bdf88ee87591e3431ed03
pendueles <contactocueto@gmail.com>
2026-06-17 16:02:11 +0200

    Create index.js

---
+++ components/screens/index.js
@@ -0,0 +1,11 @@
+// components/screens/index.js
+export { default as ProfileSelect } from "./ProfileSelect";
+export { default as BlockHomeScreen } from "./BlockHomeScreen";
+export { default as ArtistHomeScreen } from "./ArtistHomeScreen";
+export { default as ArtistProfileScreen } from "./ArtistProfileScreen";
+export { default as ProjectHomeScreen } from "./ProjectHomeScreen";
+export { default as ArtistListScreen } from "./ArtistListScreen";
+export { default as ProjectListScreen } from "./ProjectListScreen";
+export { default as ArtistCatalogueScreen } from "./ArtistCatalogueScreen";
+export { default as ClientsListScreen } from "./ClientsListScreen";
+export { default as LabelManagersListScreen } from "./LabelManagersListScreen";
