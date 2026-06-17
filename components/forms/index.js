a1fd9f465ffb853bf7a73cc3a756cec28e531663
pendueles <contactocueto@gmail.com>
2026-06-17 16:07:13 +0200

    Create index.js

---
+++ components/forms/index.js
@@ -0,0 +1,7 @@
+// components/forms/index.js
+export { default as FormScreen } from "./FormScreen";
+export { default as NewArtistForm } from "./NewArtistForm";
+export { default as ProjectForm } from "./ProjectForm";
+export { default as NewLabelManagerScreen } from "./NewLabelManagerScreen";
+export { default as ArtistEditScreen } from "./ArtistEditScreen";
+export { default as ProjectEditScreen } from "./ProjectEditScreen";
