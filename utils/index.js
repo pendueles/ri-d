42f86d49cf815802e089dedbdba76cc2f227de96
pendueles <contactocueto@gmail.com>
2026-06-20 12:48:01 +0200

    Add files via upload

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,3 @@
 // utils/index.js
 export { calcBlockScore, calcTotalScore } from "./scoring";
-export { flattenQuestions, scoreColor, scoreLabel, bgColor, compressImage, getPendingTasks } from "./helpers";
+export { flattenQuestions, scoreColor, scoreLabel, bgColor, compressImage, getPendingTasks, BLOCK_ORDER, SUBCAT_ORDER } from "./helpers";
